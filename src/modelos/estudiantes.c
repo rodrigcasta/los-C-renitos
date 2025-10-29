ff186089d7cc3a456151518644574b2264a6a67d
Nahuel Aguirre <nahueleiaguirre@gmail.com>
2025-10-28 21:49:55 -0300

    281025 - Estudiantes CRUD

---
+++ src/modelos/estudiantes.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+typedef struct Estudiante {
+  char nombre[15];
+  char apellido[15];
+  char nacimiento[11];
+  int edad;
+  int sexo;
+} Estudiante;
+
+int calcularEdad(const char* fechaNacimiento);
+
+Estudiante* NewEstudiante(char nombre[15], char apellido[15], char nacimiento[11], int sexo) {
+  Estudiante* est = (Estudiante*)malloc(sizeof(Estudiante));
+  strcpy(est->nombre, nombre);
+  strcpy(est->apellido, apellido);
+  strcpy(est->nacimiento, nacimiento);
+  est->edad = calcularEdad(nacimiento);
+  est->sexo = sexo;
+  return est;
+}
+
+const char* ObtenerNombre(const Estudiante* e) {
+    return e->nombre;
+}
+
+void CambiarNombre(Estudiante* e, const char* nombre) {
+    strcpy(e->nombre, nombre);
+}
+
+const char* ObtenerApellido(const Estudiante* e) {
+    return e->apellido;
+}
+
+void CambiarApellido(Estudiante* e, const char* apellido) {
+    strcpy(e->apellido, apellido);
+}
+
+const char* ObtenerNacimiento(const Estudiante* e) {
+    return e->nacimiento;
+}
+
+void CambiarNacimiento(Estudiante* e, const char* nacimiento) {
+    strcpy(e->nacimiento, nacimiento);
+    e->edad = calcularEdad(nacimiento);
+}
+
+int ObtenerEdad(const Estudiante* e) {
+    return e->edad;
+}
+
+int ObtenerSexo(const Estudiante* e) {
+    return e->sexo;
+}
+
+void CambiarSexo(Estudiante* e, int sexo) {
+    e->sexo = sexo;
+}
+
+void EliminarEstudiante(Estudiante* est) { 
+    free(est); 
+}
+
+static int calcularEdad(const char* fechaNacimiento) {
+  int dia, mes, anio;
+  sscanf(fechaNacimiento, "%d/%d/%d", &dia, &mes, &anio);
+
+  // Obtener la fecha actual
+  time_t t = time(NULL);
+  struct tm* fechaActual = localtime(&t);
+
+  int anioActual = fechaActual->tm_year + 1900;
+  int mesActual = fechaActual->tm_mon + 1;
+  int diaActual = fechaActual->tm_mday;
+
+  int edad = anioActual - anio;
+
+  // Si todavía no cumplió años este año, restamos 1
+  if (mesActual < mes || (mesActual == mes && diaActual < dia)) {
+    edad--;
+  }
+
+  return edad;
+}
